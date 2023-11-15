7d96ded7f6753f82316c97daf4cf67114382b0a5
Kammie Ping <kping@umd.edu>
2023-11-15 00:57:01 -0500

    draw bloons gameview

--- app/src/main/java/com/example/project7/GameView.kt
+++ app/src/main/java/com/example/project7/GameView.kt
@@ -1,22 +1,30 @@
 package com.example.project7
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.view.View
 
-class GameView {
-    private lateinit var bloon:Balloon
+class GameView : View {
+    private var bloons:ArrayList<Balloon> = ArrayList<Balloon>()
     private lateinit var paint : Paint
+    private lateinit var context : Context
 
-    constructor(newBloon:Balloon){
-        bloon = newBloon
+    constructor(context: Context, newBloons:ArrayList<Balloon>) : super(context) {
+        bloons = newBloons
         paint = Paint()
         paint.color = Color.CYAN
         paint.isAntiAlias = true
         paint.strokeWidth = 20.0f
+
+        this.context = context
     }
 
-    fun buildBloon(){
-
+    override fun onDraw(canvas:Canvas){
+        super.onDraw(canvas)
+        for (bloon in bloons){
+            canvas.drawCircle(bloon.getX().toFloat(),bloon.getY().toFloat(),bloon.getRadius().toFloat(),paint)
+        }
     }
 }
