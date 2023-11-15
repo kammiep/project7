27719949c8851389632781540a6b5ac79fc13763
Michelle Lu <mlu7@terpmail.umd.edu>
2023-11-15 00:14:27 -0500

    sax done

--- app/src/main/java/com/example/project7/SAXHandler.kt
+++ app/src/main/java/com/example/project7/SAXHandler.kt
@@ -1,7 +1,47 @@
 package com.example.project7
 
+import org.xml.sax.Attributes
 import org.xml.sax.helpers.DefaultHandler
 
 class SAXHandler : DefaultHandler() {
+    private var items : ArrayList<Balloon> = ArrayList<Balloon>()
+    private var currentItem : Balloon? = null
+    private var element : String = ""
+    private var validText : Boolean = false
 
+    override fun startElement( uri: String?, localName: String?, qName: String?, attributes: Attributes?) {
+        super.startElement(uri, localName, qName, attributes)
+        // Log.w( "MainActivity", "startElement = " + qName )
+        element = qName!!
+        validText = true
+        if( qName.equals( "balloon" ) )
+            currentItem = Balloon( 0, 0,0 )
+    }
+
+    override fun endElement(uri: String?, localName: String?, qName: String?) {
+        super.endElement(uri, localName, qName)
+        // Log.w( "MainActivity", "endElement = " + qName )
+        validText = false
+        if( qName != null && currentItem != null && qName!!.equals( "balloon" ) )
+            items.add( currentItem!! )
+    }
+
+    override fun characters(ch: CharArray?, start: Int, length: Int) {
+        super.characters(ch, start, length)
+        if( ch != null ) {
+            var text : String = String( ch!!, start, length )
+            // Log.w( "MainActivity", "text is " + text )
+
+            if( currentItem != null && validText && element.equals( "x" ) )
+                currentItem!!.setX( text.toInt() )
+            else if( currentItem != null && validText && element.equals( "y" ) )
+                currentItem!!.setY( text.toInt() )
+            else if(currentItem != null && validText && element.equals( "radius" ))
+                currentItem!!.setRadius(text.toInt())
+        }
+    }
+
+    fun getItems( ) : ArrayList<Balloon> {
+        return items
+    }
 }
