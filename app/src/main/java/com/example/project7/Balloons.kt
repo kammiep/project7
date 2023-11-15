6dcc12456b9ce1d8c18b6de17faf5957bd1c556e
Michelle Lu <mlu7@terpmail.umd.edu>
2023-11-15 01:12:40 -0500

    bloons

--- app/src/main/java/com/example/project7/Balloons.kt
+++ app/src/main/java/com/example/project7/Balloons.kt
@@ -3,6 +3,7 @@ package com.example.project7
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Canvas
+import kotlin.math.pow
 
 class Balloons {
     private var bloonsList: ArrayList<Balloon> = ArrayList<Balloon>()
@@ -13,12 +14,18 @@ class Balloons {
 
     }
 
-    fun buildBloon(){
-
-    }
-
     fun checkClicked (x : Float, y : Float) : Boolean {
-        return false
+        var ans = false
+        for (b in bloonsList) {
+            // find distance from center
+            var d : Double = Math.sqrt((x - b.getX()).pow(2) +
+                    (y - b.getY()).pow(2).toDouble())
+            // check if distance is less than radius
+            if (d < b.getRadius().toDouble()) {
+                ans = true
+            }
+        }
+        return ans
     }
 
     fun getList() : ArrayList<Balloon> {
